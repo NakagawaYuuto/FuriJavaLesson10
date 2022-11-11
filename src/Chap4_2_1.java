7a3d64c1183633017ae5ed6d3beeb2b441b025cc
NakagawaYuuto <github.nakagawa@gmail.com>
2022-11-11 18:05:59 +0900

    ゲッター・セッター

---
+++ src/Chap4_2_1.java
@@ -0,0 +1,9 @@
+public class Chap4_2_1 {
+    public static void main(String[] args) {
+        Chara chara1 = new Chara("剣士");
+        Chara chara2 = new Chara("魔法使い");
+
+        chara1.sayJob();
+        chara2.sayJob();
+    }
+}
